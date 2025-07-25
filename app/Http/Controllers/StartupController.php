e693439550ad9f5ff0e422f4760736cc6271a5d5
Shirley Kuik <kuik0224@gmail.com>
2025-07-25 15:51:42 +0800

    add validation to startup registration

--- app/Http/Controllers/StartupController.php
+++ app/Http/Controllers/StartupController.php
@@ -34,12 +34,43 @@ class StartupController extends Controller
 
     public function store(Request $request): JsonResponse
     {
+        $malaysianStates = [
+            'Johor', 'Kedah', 'Kelantan', 'Melaka', 'Negeri Sembilan', 'Pahang',
+            'Penang', 'Perak', 'Perlis', 'Sabah', 'Sarawak', 'Selangor', 'Terengganu',
+            'Kuala Lumpur', 'Labuan', 'Putrajaya'
+        ];
+
         $validated = $request->validate([
             'name' => 'required|string|max:255',
-            'contact_no' => 'required|string|max:20',
+            'contact_no' => [
+                'required',
+                'string',
+                'max:20',
+                function ($attribute, $value, $fail) {
+                    if (strpos($value, '+60') !== 0) {
+                        $fail('Your company is not eligible to use our platform.');
+                    }
+                }
+            ],
             'company_name' => 'required|string|max:255',
             'company_sector' => 'required|string|max:100',
-            'company_address' => 'required|string|max:255',
+            'company_address' => [
+                'required',
+                'string',
+                'max:255',
+                function ($attribute, $value, $fail) use ($malaysianStates) {
+                    $found = false;
+                    foreach ($malaysianStates as $state) {
+                        if (stripos($value, $state) !== false) {
+                            $found = true;
+                            break;
+                        }
+                    }
+                    if (!$found) {
+                        $fail('Your company is not eligible to use our platform.');
+                    }
+                }
+            ],
             'email' => 'required|email|unique:users',
             'password' => 'required|min:6'
         ]);
